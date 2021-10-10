90baaef7023c801c9342ddcaf7ac541fd40ac01c
FPtrevi <82823817+FPtrevi@users.noreply.github.com>
2021-10-11 01:12:05 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,51 +1,51 @@
-const list = [
+var list = [
     {
-        name: "조현채"   ,
+        id: "조현채"   ,
         university :  "부산여대"   ,
         department : "호텔외식조리학과"
     },
     {
-        name: "백인서"   ,
+        id: "백인서"   ,
         university :  "부산여대"   ,
         department : "호텔외식조리학과"
     },
     {
-        name: "최수연"   ,
+        id: "최수연"   ,
         university :  "부산여대"   ,
         department : "사회복지학과"
     },
     {
-        name: "박소연"   ,
+        id: "박소연"   ,
         university :  "동의과학대"   ,
         department : "응급구조과"
     },
     {
-        name: "박유진"   ,
+        id: "박유진"   ,
         university :  "동의대"   ,
         department : "보육가정상담학과"
     },
     {
-        name: "박지은"   ,
+        id: "박지은"   ,
         university :  "동의과학대"   ,
         department : "간호학과"
     },
     {
-        name: "김상은"   ,
+        id: "김상은"   ,
         university :  "동의대"   ,
         department : "국제관광경영학과"
     },
     {
-        name: "이창민"   ,
+        id: "이창민"   ,
         university :  "동의대"   ,
         department : "보육가정상담학과"
     },
     {
-        name: "박민지"   ,
+        id: "박민지"   ,
         university :  "동의과학대"   ,
         department : "방사선과"
     },
     {
-        name: "손용환"   ,
+        id: "손용환"   ,
         university :  "동의과학대"   ,
         department : "공간정보지적과"
     },
@@ -63,20 +63,13 @@ function go_random(){
     let a = rank.length;
     rand = Math.floor(Math.random()*a); //0~9
     num = rank[rand];
-    names = list[num].name
-    universitys = list[num].university
-    departments = list[num].department
-    console.log(list[num].name);
+
+    names = list[num].id;
+    universitys = list[num].university;
+    departments = list[num].department;
+    console.log(list[num].id);
     rank.splice(rank.indexOf(num),1);
 }
-
-// if(){
-//     rank.push(rand)
-// }
-
-// console.log(rand)
-
-// console.log(list[rand].name, list[rand].department)
 
 const ones = document.querySelector("#one");
 const twos = document.querySelector("#two");
@@ -91,11 +84,11 @@ const tsts = document.querySelector("#three_rd")
 const tsts1 = document.querySelector("#three_rd1")
 const tsts2 = document.querySelector("#three_rd2")
 
-const check = ""
-
+stop1=true
 
 function one_event(event){
     event.preventDefault();
+    clearInterval(past1);
     go_random();
     fsts.innerText = `${names}\n${universitys}\n${departments}`;
 }
@@ -105,9 +98,12 @@ function two_event(event){
     go_random();
     console.log(ssts.value)
     if(ssts.value == undefined){
+        clearInterval(past2);
         ssts.innerText = `${names}\n${universitys}\n${departments}`;
         ssts.value=true
+        
     }else{
+        clearInterval(past2_1);
         ssts1.innerText = `${names}\n${universitys}\n${departments}`;
     }
 }
@@ -116,15 +112,88 @@ function three_event(event){
     event.preventDefault();
     go_random();
     if(tsts.value == undefined){
+        clearInterval(past3);
         tsts.innerText = `${names}\n${universitys}\n${departments}`;
         tsts.value=true
     }else if(tsts1.value == undefined){
+        clearInterval(past3_1);
         tsts1.innerText = `${names}\n${universitys}\n${departments}`;
         tsts1.value=true
     }else{
+        clearInterval(past3_2);
         tsts2.innerText = `${names}\n${universitys}\n${departments}`;
     }
 }
+
+
+
+randomId=""
+randomUniversity=""
+randomDepartment=""
+getNumber=0
+
+function getRandom1(getNumber){
+    getNumber = Math.floor(Math.random()*10);
+    randomId = list[getNumber].id;
+    randomUniversity = list[getNumber].university;
+    randomDepartment = list[getNumber].department;
+    fsts.innerText = `${randomId}\n${randomUniversity}\n${randomDepartment}`;
+}
+
+function getRandom2(getNumber){
+    getNumber = Math.floor(Math.random()*10);
+    randomId = list[getNumber].id;
+    randomUniversity = list[getNumber].university;
+    randomDepartment = list[getNumber].department;
+    ssts.innerText = `${randomId}\n${randomUniversity}\n${randomDepartment}`;
+}
+
+function getRandom2_1(getNumber){
+    getNumber = Math.floor(Math.random()*10);
+    randomId = list[getNumber].id;
+    randomUniversity = list[getNumber].university;
+    randomDepartment = list[getNumber].department;
+    ssts1.innerText = `${randomId}\n${randomUniversity}\n${randomDepartment}`;
+}
+
+function getRandom3(getNumber){
+    getNumber = Math.floor(Math.random()*10);
+    randomId = list[getNumber].id;
+    randomUniversity = list[getNumber].university;
+    randomDepartment = list[getNumber].department;
+    tsts.innerText = `${randomId}\n${randomUniversity}\n${randomDepartment}`;
+}
+
+function getRandom3_1(getNumber){
+    getNumber = Math.floor(Math.random()*10);
+    randomId = list[getNumber].id;
+    randomUniversity = list[getNumber].university;
+    randomDepartment = list[getNumber].department;
+    tsts1.innerText = `${randomId}\n${randomUniversity}\n${randomDepartment}`;
+}
+
+function getRandom3_2(getNumber){
+    getNumber = Math.floor(Math.random()*10);
+    randomId = list[getNumber].id;
+    randomUniversity = list[getNumber].university;
+    randomDepartment = list[getNumber].department;
+    tsts2.innerText = `${randomId}\n${randomUniversity}\n${randomDepartment}`;
+}
+
+// function deleyOne(event){
+//     event.preventDefault();
+//     console.log(list[getNumber].id)
+//     setInterval(getRandom,50);
+// }
+
+let past1 = setInterval(getRandom1,50);
+let past2 = setInterval(getRandom2,50);
+let past2_1 = setInterval(getRandom2_1,50);
+let past3 = setInterval(getRandom3,50);
+let past3_1 = setInterval(getRandom3_1,50);
+let past3_2 = setInterval(getRandom3_2,50);
+
+
 
 ones.addEventListener("submit", one_event);
 twos.addEventListener("submit", two_event);
